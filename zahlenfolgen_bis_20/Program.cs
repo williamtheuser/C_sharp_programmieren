1649050ef4e7029f01c7413b9db6b0e30a7d1457
williamtheuser <william.copellini@outlook.de>
2023-09-05 15:52:02 +0200

    aufgabe 7.1_F1

---
+++ zahlenfolgen_bis_20/Program.cs
@@ -0,0 +1,41 @@
+﻿int[] naturalNubers = new int[20];
+int[] evenNumbers = new int[20];
+int[] squareNumbers = new int[20];
+
+
+
+
+
+for (int i = 0; i < 20; i++)
+{
+    naturalNubers[i] = i;
+
+}
+
+
+int counter = 0;
+int indexCounter = 0;
+while (true)
+{
+    if (counter % 2 == 0)
+    {
+        evenNumbers[indexCounter] = counter;
+        indexCounter++;
+    }
+    counter++;
+    if (evenNumbers[evenNumbers.Length-1] !=0)
+    {
+        break;
+    }
+}
+
+
+for (int k = 0; k < 20; k++)
+{
+    squareNumbers[k] = k * k;
+}
+
+Console.WriteLine(string.Join(",", naturalNubers));
+Console.WriteLine(string.Join(",", evenNumbers));
+Console.WriteLine(string.Join(",", squareNumbers));
+        
