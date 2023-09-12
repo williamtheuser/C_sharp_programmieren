36c1ce5b1264275d9c619b9136699f45d6236ee4
williamtheuser <william.copellini@outlook.de>
2023-09-12 16:22:48 +0200

    Aufgaben G teilweise hinzugefügt

---
+++ Funktionen/Program.cs
@@ -0,0 +1,33 @@
+﻿//Aufgaben G
+
+static void ATE()
+{
+    Console.WriteLine("42");
+}
+
+ATE();
+
+static int sum(int a, int b)
+{
+    return a + b;
+}
+
+Console.WriteLine(sum(3, 6));
+
+static bool isEven(int x)
+{
+    if (x % 2 ==0)
+    {
+        return true;
+    }
+
+    else
+    {
+        return false;
+    }
+}
+
+Console.WriteLine(isEven(34));
+
+//G2
+
