f338b7cf194a08783d715fb7bef5c20b0579c240
williamtheuser <william.copellini@outlook.de>
2023-09-12 14:40:47 +0200

    .gitnore von c auf c#, dateien hinzugefügt

---
+++ Fibonacci bis n/Program.cs
@@ -0,0 +1,4 @@
+﻿//A1-A6
+//Konnte sie nicht mehr finden und schreibe diese definitiv nicht nocheinmal weil: zu einfach
+
+//B
---
+++ Text_cleaner_und_string_Methoden/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+
+
+bool userToTired = true;
+
+
+string a;
+
+
+while (userToTired) {
+     a = Console.ReadLine();
+    if (a is string)
+    {
+        break;
+    }
+    continue;
+    
+    
+}
+
+
