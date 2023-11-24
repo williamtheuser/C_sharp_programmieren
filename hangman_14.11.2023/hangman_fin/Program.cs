1a7adfc21e78e0bce2845c0a7301497cf9844610
williamtheuser <william.copellini@outlook.de>
2023-11-24 19:44:01 +0100

    Hangman 24.11.2023

---
+++ hangman_14.11.2023/hangman_fin/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+
+namespace HangMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string secretWord = ReadSecretWord();
+                int allowedErrors = CalculateAllowedErrors(secretWord.Length);
+                int incorrectGuesses = 0;
+
+                HangTheMan(incorrectGuesses, allowedErrors);
+
+                while (true)
+                {
+                    char guessedChar = ReadOneChar();
+                    bool isCorrectGuess = EvaluateTheSituation(secretWord, guessedChar);
+
+                    if (!isCorrectGuess)
+                    {
+                        incorrectGuesses++;
+                        if (incorrectGuesses >= allowedErrors)
+                            break;
+                    }
+
+                    HangTheMan(incorrectGuesses, allowedErrors);
+
+                    if (secretWord.All(c => guessedChar == c))
+                    {
+                        Console.WriteLine("Congratulations! You guessed the word: " + secretWord);
+                        break;
+                    }
+                }
+
+                QuitOrRestart();
+            }
+        }
+
+        static string ReadSecretWord()
+        {
+            string secretWord;
+
+            do
+            {
+                Console.Write("Player 1, enter the secret word (at least 3 letters, only uppercase ASCII): ");
+                secretWord = Console.ReadLine().ToUpper();
+            } 
+            
+            while (string.IsNullOrWhiteSpace(secretWord) || secretWord.Length < 3 || !IsValidWord(secretWord));
+
+            return secretWord;
+        }
+
+        static bool IsValidWord(string word)
+        {
+            foreach (char c in word)
+            {
+                if (c < 'A' || c > 'Z')
+                    return false;
+            }
+            return true;
+        }
+
+        static int CalculateAllowedErrors(int wordLength)
+        {
+            return (int)(0.75 * wordLength);
+        }
+
+        static char ReadOneChar()
+        {
+            char guessedChar;
+
+            do
+            {
+                Console.Write("Player 2, enter your guess: ");
+                char.TryParse(Console.ReadLine()?.ToUpper(), out guessedChar);
+            } while (!IsValidGuess(guessedChar));
+
+            return guessedChar;
+        }
+
+        static bool IsValidGuess(char guessedChar)
+        {
+            return guessedChar >= 'A' && guessedChar <= 'Z';
+        }
+
+        static bool EvaluateTheSituation(string secretWord, char guessedChar)
+        {
+            bool isCorrectGuess = secretWord.Contains(guessedChar);
+            
+            if (!isCorrectGuess)
+                Console.WriteLine($"Incorrect guess: {guessedChar}");
+
+            return isCorrectGuess;
+        }
+
+        static void HangTheMan(int incorrectGuesses, int allowedErrors)
+        {
+            int frameStep = (int)(15.0 / allowedErrors);
+            int currentFrame = Math.Min(incorrectGuesses * frameStep, 14);
+
+            Console.Clear();
+            Console.WriteLine(hangmanFrames[currentFrame]);
+        }
+
+        static void QuitOrRestart()
+        {
+            Console.Write("Do you want to play again? (Y/N): ");
+            char restartChoice = Console.ReadKey().KeyChar;
+            if (char.ToUpper(restartChoice) != 'Y')
+            {
+                Environment.Exit(0);
+            }
+        }
+
+        static string[] hangmanFrames = 
+        {
+            
+    // Frame 0
+    @"_________
+|/      |
+|
+|
+|
+|
+|",
+    // Frame 1
+    @"_________
+|/      |
+|      (_)
+|
+|
+|
+|",
+    // Frame 2
+    @"_________
+|/      |
+|      (_)
+|       |
+|
+|
+|",
+    // Frame 3
+    @"_________
+|/      |
+|      (_)
+|      /|
+|
+|
+|",
+    // Frame 4
+    @"_________
+|/      |
+|      (_)
+|      /|\
+|
+|
+|",
+    // Frame 5
+    @"_________
+|/      |
+|      (_)
+|      /|\
+|      /
+|
+|",
+    // Frame 6
+    @"_________
+|/      |
+|      (_)
+|      /|\
+|      / \
+|
+|"
+    // Frame 7
+};
+
+        };
+    }
+}
---
+++ hangman_redone/ConsoleApp1/Program.cs
@@ -0,0 +1,135 @@
+﻿namespace HangMan; // here use name of your project
+
+class Program
+{
+    // No variable declarations in this area!!
+    static void Main(string[] args)
+    {
+        string abc = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        string secretWord;
+        while (true)                     // The game repeats until finished by player 1
+        {
+            // Variable declarations allowed here
+            ReadSecretWord(ref abc);            // Player 1: Enter the secret word to be guessed by player 2
+            HangTheMan();                // Screen output for a good start
+            while (true)                 // Player 2: Make your guesses
+            {
+                ReadOneChar(ref abc);           // Handle input of one char. 
+                EvaluateTheSituation();  // Game Logic goes here
+                HangTheMan();            // Screen output goes here
+            }
+            if (QuitOrRestart() == "restart")
+            {
+                continue;
+            }
+
+            else if (QuitOrRestart() == "quit")
+            {
+                break;
+            }
+             // Ask if want to quit or start new game
+        }
+    }
+
+    static string ReadSecretWord(ref string abc, ref string secretWord)
+    {
+        Console.WriteLine("Type the word to be guessed!");
+        bool pass = false;
+        secretWord = Console.ReadLine();
+        while (pass == false)
+        {
+            secretWord = Console.ReadLine();
+            int matchCounter = 0;
+            foreach (char currentCharInput in secretWord)
+            {   
+                if (abc.Contains(currentCharInput) == false )
+                {
+                    pass = false;
+                }
+                else
+                {
+                    matchCounter++;
+                    if (matchCounter == secretWord.Length && secretWord != "")
+                    {
+                        pass = true; 
+                    }
+                }
+            } 
+        }
+        return secretWord;
+    }
+
+    static string ReadOneChar(ref string abc) // Modification of method declaration recommended: Add return value and parameters
+                              // If there are rules and constraints on allowed secrets (e.g. no digits), make sure the input is allowed
+    {
+        string currentOneChar = Console.ReadKey().ToString();
+        bool pass = false;
+        while (pass == false) 
+        { 
+            if (abc.Contains(currentOneChar))
+            {
+                break;
+            }
+
+            else
+            {
+                    continue;
+            }
+
+        }
+        return currentOneChar;
+    }
+
+    static void EvaluateTheSituation() // Modification of method declaration recommended: Add return value and parameters
+                               // In here, evaluate the char entered: Is it part of the secret word?
+                                       // Calculate and return the game status (Hit or missed? Where? List and number of missed chars?...)
+    {
+        // Variable declarations allowed here
+        // NO Console.Write() etc. in here!
+    }
+
+    static string QuitOrRestart() // Modification of method declaration recommended: Add return value and parameters
+        // If there are rules and constraints on allowed secrets (e.g. no digits), check them in here
+    {
+        Console.WriteLine("quit / restart (Q / N)?");
+        string nxtCommand = Console.ReadLine().ToString();
+        bool pass = false;
+        while (true) 
+        { 
+            if ("QRqr".Contains(nxtCommand))
+                {
+                    pass = true;
+                }
+
+            else
+                {
+                    continue;
+                }
+
+        }
+
+        if (nxtCommand == "r" || nxtCommand == "R")
+        {
+            return "restart";
+        }
+
+        else if (nxtCommand == "q" || nxtCommand == "Q")
+        {
+            return "replay";
+        }
+
+    }
+
+    static string[] HangTheMan()
+    {
+        int maxErrors = (int)(secretWord.Length * 0.6);
+        string[] frames = new string[maxErrors + 1];
+        for (int i = 0; i <= maxErrors; i++)
+        {
+            string frameFilePath = $"frame{i}.txt";
+            frames[i] = System.IO.File.ReadAllText(frameFilePath);
+        }
+        return frames;
+    }
+}
+
